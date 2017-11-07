14fca7eb6004d89746a68bc73e6f8898e9c3f62b
Xoberti1 <xoberti@gmail.com>
2017-11-06 19:25:53 -0600

    Lys stuff

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-
-	var config = {
-	  	apiKey: "AIzaSyAH08gCmyuhWsZ-2HLKIatrYzH3iRYbnyc",
-	    authDomain: "mytravelpal-ebf41.firebaseapp.com",
-	    databaseURL: "https://mytravelpal-ebf41.firebaseio.com",
-	    projectId: "mytravelpal-ebf41",
-	    storageBucket: "mytravelpal-ebf41.appspot.com",
-	    messagingSenderId: "287994305952"
-	};
-
-	firebase.initializeApp(config);
-
-    var map;
-    var infoWindow;
-    var service;
-    var database = firebase.database();
-    var auth = firebase.auth();
-    var user = firebase.auth().currentUser;
-    var email = $("#email");
-  	var password = $("#password");
-  	var login = $("#login");
-  	var signup = $("#signup");
-  	var logout = $("#logout");
-  	var signupsub = $("#signupsubmit");
-  	var loginsub = $("#loginsubmit");
-  	var userlocation = $("#userlocation");
-  	var calendar = $("#calendar");
-    var submit = $("#submit");
-  	var events = $("#events");
-  	var attractions = $("#attractions");
-  	var restaurants = $("#restaurants");
-  	var eventfulAPI = "app_key=Qm9xNFv7PP2fqZVZ";
-  	var eventfulURL = "https://api.eventful.com/json/events/search?";
-    var googleAPI = "AIzaSyCmuGjtB5AKU9b4DFOD3c6m7g2I4jlTP_4";
-    var googleURL = "https://maps.googleapis.com/maps/api/js?key=";
-	 //var queryURL = "http://api.eventful.com/rest/events/search?" + "app_key=Qm9xNFv7PP2fqZVZ&" + "Houston" + "&" + "books";
-		var eventfulQuery = "https://api.eventful.com/json/events/search?keywords=music&location=Singapore&app_key=Qm9xNFv7PP2fqZVZ";
-    var googleQuery = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCmuGjtB5AKU9b4DFOD3c6m7g2I4jlTP_4&callback=initMap&libraries=places,visualization";
-    console.log(eventfulQuery);
-    console.log(googleQuery);
-
-    	$.ajax({
-        url: eventfulQuery,
-        method: "GET",
-        dataType: 'jsonp'
-      }).done(function(response) {
-          console.log(response);
-      });
-
-      $.ajax({
-        url: googleQuery,
-        method: "GET",
-        dataType: 'jsonp'
-      }).done(function(response) {
-          console.log(response);
-      });
-=======
 $(document).ready(function() {
 
   var config = {
@@ -101,7 +42,6 @@ $(document).ready(function() {
       console.log(response);
     });
   });
->>>>>>> 20b22b0f76db380b309561d112171d4b6a4e54e2
 
   logout.hide();
 
@@ -141,14 +81,6 @@ $(document).ready(function() {
     console.log(pass);
     //console.log(displayname);
     //Sign-in
-<<<<<<< HEAD
-    auth.createUserWithEmailAndPassword(emailval, pass).then(function(){
-      window.location.href="locationselector.html";
-    })
-    .catch(function(error){
-      console.log('sign-in error', error)
-    })
-=======
     auth.createUserWithEmailAndPassword(emailval, pass).then(function() {
         window.location.href = "locationselector.html";
       })
@@ -156,13 +88,11 @@ $(document).ready(function() {
         console.log('sign-in error', error)
       })
     //window.location.href="locationselector.html";
->>>>>>> 20b22b0f76db380b309561d112171d4b6a4e54e2
   });
 
   //logout event
   logout.on('click', function() {
     firebase.auth().signOut();
-    window.location.href="homepage.html";
   });
 
   //make sure the user is a user
@@ -174,23 +104,6 @@ $(document).ready(function() {
       console.log(email);
       console.log(uid);
       logout.show();
-<<<<<<< HEAD
-    }
-    else{
-      console.log("not a user");
-      logout.hide();
-    };
-  });
-
-  submit.on("click", function(){
-    var locationVal = userlocation.val();
-    var calendarVal = calendar.val();
-
-    console.log(locationVal);
-    console.log(calendarVal);
-
-    window.location.href="eventselector.html";
-=======
       //window.location.href="locationselector.html";
 
     } else {
@@ -225,13 +138,11 @@ $(document).ready(function() {
     });
   });
 
-
   submit.on("click", function() {
     var locationVal = userlocation.val();
     var calendarVal = calendar.val();
     console.log(locationVal);
     console.log(calendarVal);
     window.location.href = "eventselector.html";
->>>>>>> 20b22b0f76db380b309561d112171d4b6a4e54e2
   });
 });
